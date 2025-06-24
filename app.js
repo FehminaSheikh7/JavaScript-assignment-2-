8ca4dc202f80af5689aefb90101ac7a4de53b956
Fehmina Sheikh <fehminasheikh10@gmail.com>
2025-06-24 19:13:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let username;
+let myName = "Fehmina Sheikh";
+let message;
+message = "Hello World!"
+alert(message);
+let studentName = "Fehmina Sheikh";
+let age = "27";
+let course = "Web Developement course";
+
+alert(studentName);
+alert(age);
+alert(course);
+let food = "pizza\npizz\npiz\npi\np";
+alert(food)
+let email = "Fehminasheikh10@gmail.com";
+alert("my email address is  " + email);
+let book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book A smarter way to learn javascrit" + book);
+document.writeln("Yah! I can write HTML content through JavaScript <br>");
+let show = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+alert(show)
+document.writeln(show)
